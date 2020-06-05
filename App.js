500cfaf48549a74323d79c2c5a86c407933dc1f7
Michael Chen <michaelchengaming@gmail.com>
2020-06-04 22:50:14 -0700

    delete stack nav for drawer nav, add details and home screen

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+// import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import HomeScreen from "./src/containers/HomeScreen";
+import DetailsScreen from "./src/containers/DetailsScreen";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  // console.log("hello");
   return (
-    <View style={styles.container}>
-      <StatusBar backgroundColor="plum" barStyle="dark-content" />
-      <Text style={styles.text}>Open up App.js to start working on your new app!</Text>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Details" component={DetailsScreen} />
+        {/* need to rename details to something else */}
+        {/* <Drawer.Screen name="Details" component={DetailsScreen} /> */}
+        {/* <Drawer.Screen name="Details" component={DetailsScreen} /> */}
+      </Drawer.Navigator>
+      {/* <View style={styles.container}>
+          <StatusBar backgroundColor="plum" barStyle="dark-content" />
+          <Text style={styles.text}>Open up App.js to start working on your new app!</Text>
+        </View> */}
+      {/* <Stack.Navigator> */}
+      {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: "deez nuts" }} /> */}
+      {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
+      {/* </Stack.Navigator> */}
+    </NavigationContainer>
   );
 }
 
