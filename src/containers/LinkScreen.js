6f663adc1b5375a6d9f353685fb20409121305b0
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-06-05 00:30:58 -0700

    Link Screen

---
+++ src/containers/LinkScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const LinkScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Link Screen</Text>
+    </View>
+  );
+};
+
+export default LinkScreen;
