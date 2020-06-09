14b6aa8d9affd403743465dfd4067f000f161745
Michael Chen <michaelchengaming@gmail.com>
2020-06-08 21:09:29 -0700

    add ghetto header

--- App.js
+++ App.js
@@ -10,8 +10,6 @@ import HomeScreen from "./src/containers/HomeScreen";
 import DetailsScreen from "./src/containers/DetailsScreen";
 import LinkScreen from "./src/containers/LinkScreen";
 
-// import Supereme from "";
-
 const Drawer = createDrawerNavigator();
 
 export default function App() {
@@ -29,35 +27,28 @@ export default function App() {
           initialRouteName="Home"
           drawerStyle={{
             backgroundColor: "#4C566A",
-            // width: "75%",
           }}
           overlayColor="transparent"
-          // hideStatusBar={true}
           drawerContentOptions={{
             activeTintColor: "plum",
             itemStyle: { marginVertical: 3 },
             labelStyle: {
               padding: 3,
               fontSize: 20,
-              // fontWeight: "bold",
               fontFamily: "Supreme",
             },
           }}
         >
-          <Drawer.Screen name="Home" component={HomeScreen} />
-          {/* <DrawerItem */}
+          <Drawer.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headTitle: "titties",
+            }}
+          />
           <Drawer.Screen name="Details" component={DetailsScreen} />
-          {/* need to rename details to something else */}
           <Drawer.Screen name="Links" component={LinkScreen} drawerStyle={styles.label} />
-          {/* <Drawer.Screen name="Details" component={DetailsScreen} /> */}
         </Drawer.Navigator>
-        {/* <View style={styles.container}>
-          <Text style={styles.text}>Open up App.js to start working on your new app!</Text>
-        </View> */}
-        {/* <Stack.Navigator> */}
-        {/* <Stack.Screen name="Home" component={HomeScreen} options={{ title: "deez nuts" }} /> */}
-        {/* <Stack.Screen name="Details" component={DetailsScreen} /> */}
-        {/* </Stack.Navigator> */}
       </NavigationContainer>
     );
   }
@@ -67,9 +58,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#4C566A",
-    // color: "white",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   label: {
     color: "white",
--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-    </View>
+    <>
+      <View style={{ backgroundColor: "green", width: "100%", height: "10%" }} />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+      </View>
+    </>
   );
 };
 
