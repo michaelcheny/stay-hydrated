01820f57d59f87efb7db44d8f51772b7e4f80d54
Michael Chen <michaelchengaming@gmail.com>
2020-06-08 22:37:26 -0700

    link button thing works

--- src/containers/LinkScreen.js
+++ src/containers/LinkScreen.js
@@ -1,13 +1,50 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import * as Linking from "expo-linking";
 
 const LinkScreen = () => {
+  // const handlePress = (e) => {
+  //   console.log(e);
+  //   // Linking.openURL();
+  //   // this.props.onPress && this.props.onPress();
+  // };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Link Screen</Text>
-      <Text>https://water4.org/</Text>
-      <Text>https://fightfortheforgotten.org/</Text>
-    </View>
+    <>
+      <Image
+        source={require("../../assets/foundationlogo.png")}
+        style={
+          {
+            // display: "flex",
+            // justifyContent: "center",
+          }
+        }
+      />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ marginBottom: 20 }}>Link Screen</Text>
+        <View
+          style={{
+            backgroundColor: "aquamarine",
+            padding: 20,
+            paddingHorizontal: 100,
+            borderRadius: 20,
+          }}
+          onPress={() => {
+            Linking.openURL("https://water4.org/");
+          }}
+        >
+          <Text
+            style={{
+              color: "purple",
+            }}
+          >
+            https://water4.org/
+          </Text>
+        </View>
+
+        <Text>https://fightfortheforgotten.org/</Text>
+      </View>
+    </>
   );
 };
 
