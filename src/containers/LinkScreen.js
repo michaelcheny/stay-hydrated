8f4e2cf73d6f68b85baa0d7972e5171c7e4365ab
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-06-08 21:59:19 -0700

    add links

--- src/containers/LinkScreen.js
+++ src/containers/LinkScreen.js
@@ -5,6 +5,8 @@ const LinkScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Link Screen</Text>
+      <Text>https://water4.org/</Text>
+      <Text>https://fightfortheforgotten.org/</Text>
     </View>
   );
 };
